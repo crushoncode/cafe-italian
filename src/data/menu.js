da57fa18ef5f93940336c1c56f9be2a80b909739
crushoncode <anandnareshit@gmail.com>
2025-10-27 07:20:09 +0530

    fifth commit

--- src/data/menu.js
+++ src/data/menu.js
@@ -11,7 +11,7 @@ const menu = [
     name: "Margherita",
     description: "Classic pizza with fresh basil.",
     price: 399,
-    image: "https://i.pinimg.com/736x/5a/21/84/5a2184cf3268f5321481f506ffaceaf7.jpg"
+    image: "https://st.depositphotos.com/1177973/2397/i/450/depositphotos_23970469-stock-photo-girl-housewife-with-delicious-pizza.jpg"
   },
   
 ];
